629eac983ef4210f065f04d4cd70d23be2edbc9d
전지영 <jeeyoung@Jeeyoung-MacBook-Air.local>
2025-03-31 10:22:39 +0900

    ver1.8

--- run.py
+++
@@ -1,8 +0,0 @@
-import os
-import subprocess
-
-# Render가 제공하는 PORT 환경변수를 읽음
-port = os.environ.get("PORT", "8000")
-
-# Chainlit 실행 (추가 옵션 없이)
-subprocess.run(["chainlit", "run", "main.py"], env={**os.environ, "PORT": port})
