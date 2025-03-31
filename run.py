190efd49e76db14fd6881e84ea648656d01fe015
전지영 <jeeyoung@Jeeyoung-MacBook-Air.local>
2025-03-31 10:00:11 +0900

    ver1.6

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ import os
 import subprocess
 
 port = os.getenv("PORT", "8000")  # Render가 자동 주입함
-subprocess.run(["chainlit", "run", "main.py", "-h", "0.0.0.0", "-p", port])
+subprocess.run(["chainlit", "run", "main.py", "-h", "0.0.0.0"])
